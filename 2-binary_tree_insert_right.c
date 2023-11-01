1d9e4e8f37066a7835103f2ae954dffaf8e6dfcb
sara-dme <s.daoumy@gmail.com>
2023-11-01 09:35:30 -0700

    fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,19 +12,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
+
 	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
 	node->parent = parent;
 	node->left = NULL;
-	node->right = NULL;
+	node->right = parent->right;
+	parent->right = node;
 	node->n = value;
 
-	if (parent->right == NULL)
-		parent->right = node;
-	else
-	{
-		node->right = parent->right;
-		parent->right = node;
+	if (node->right)
 		node->right->parent = node;
-	}
 	return (node);
 }
