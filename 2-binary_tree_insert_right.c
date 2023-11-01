484c1044e4454f6f448c1e8db8b969544697d9bc
sara-dme <s.daoumy@gmail.com>
2023-11-01 02:58:57 -0700

    betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,30 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -  inserts a node as the right-child of another node
+ * binary_tree_insert_right - inserts a node as the right-child
  * @parent: pointer to the node to insert
  * @value: value to store in the new node
- * Return: pointer to the created node or NULL on faillure or if the parent is NULL
+ * Return: pointer to the created node or NULL on faillure
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = NULL;
+	binary_tree_t *node = NULL;
 
-    if (parent == NULL)
-        return (NULL);
-    node = malloc(sizeof(binary_tree_t));
-    node->parent = parent;
-    node->left = NULL; 
-    node->right = NULL;
-    node->n = value;
+	if (parent == NULL)
+		return (NULL);
+	node = malloc(sizeof(binary_tree_t));
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	node->n = value;
 
-    if (parent->right == NULL)
-        parent->right = node;
-    else
-    {
-        node->right = parent->right;
-        parent->right = node;
-        node->right->parent = node;
-    }
-    return (node);
+	if (parent->right == NULL)
+		parent->right = node;
+	else
+	{
+		node->right = parent->right;
+		parent->right = node;
+		node->right->parent = node;
+	}
+	return (node);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,13 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left, right;
+	size_t left, right;
 
-    if (!tree)
-        return (0);
-    if (!tree->left && !tree->right)
-        return (0);
-    left = binary_tree_height(tree->left) + 1;
-    right = binary_tree_height(tree->right) + 1;
-    return (left > right ? left : right )
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	left = binary_tree_height(tree->left) + 1;
+	right = binary_tree_height(tree->right) + 1;
+	return (left > right ? left : right);
 }
